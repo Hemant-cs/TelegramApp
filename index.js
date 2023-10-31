9f92bd24b5dcfb9ecf01b7d52862cd8e6c7715d2
Hemant CS <62788096+Hemant-cs@users.noreply.github.com>
2023-10-31 18:53:22 +0530

    putting comments

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { Telegraf } = require("telegraf");
 
-const bot = new Telegraf("6322543019:AAFyGO8b3qMCacsqDco8m48GGISIZPb9Ovg");
+const bot = new Telegraf("6322543019:AAFyGO8b3qMCacsqDco8m48GGISIZPb9Ovg"); //a access key for chat bot
 
-bot.start((ctx) => ctx.reply(`Welcome to Hemant's Chat Bot`));
+//link of chat bot
+//https://web.telegram.org/k/#@HemantChatBot
 
-bot.command("sticker", (ctx) => ctx.reply("👍"));
+bot.start((ctx) => ctx.reply(`Welcome to Hemant's Chat Bot`)); //reply of /start
 
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.command("sticker", (ctx) => ctx.reply("👍")); //reply of all sticker
+
+bot.hears("hi", (ctx) => ctx.reply("Hey there")); //reply for hi
 
 bot.launch();
